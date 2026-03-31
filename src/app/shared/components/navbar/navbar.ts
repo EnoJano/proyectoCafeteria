61137e75e122661b179dfe87c727621715e80ed2
Alejandro F. Escobedo Miño <a.franco.escobedo@gmail.com>
2026-03-31 15:51:49 -0300

    nav link hacia opciones
    navega a inicio menu y como llegar, con nav link

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Theme } from '../../../core/services/theme';
 import { TranslateModule } from '@ngx-translate/core';
 import { Language } from '../../../core/services/language';
@@ -13,7 +13,7 @@ import { Language } from '../../../core/services/language';
 export class Navbar {
   constructor(public themeService: Theme, public languageService: Language) { }
 
-  
+
   pulseActive = false;
 
   toggleTheme() {
@@ -28,6 +28,41 @@ export class Navbar {
 
   toggleLanguage() {
     this.languageService.toggleLanguage();
+  }
+
+  activeSection: string = 'inicio';
+
+  scrollTo(section: string) {
+    const element = document.getElementById(section);
+
+    if (!element) return;
+
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    const navbarHeight = navbar?.offsetHeight || 80;
+
+    const y = element.getBoundingClientRect().top + window.scrollY - navbarHeight;
+
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth'
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const sections = ['inicio', 'menu', 'mapa'];
+
+    for (let section of sections) {
+      const element = document.getElementById(section);
+
+      if (element) {
+        const rect = element.getBoundingClientRect();
+
+        if (rect.top <= 100 && rect.bottom >= 100) {
+          this.activeSection = section;
+        }
+      }
+    }
   }
 
 }
