93d43600fcac393fac6afd3075525cb01deeb45d
Alejandro F. Escobedo Miño <a.franco.escobedo@gmail.com>
2026-03-31 18:32:05 -0300

    footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Language } from '../../../core/services/language';
+import { Theme } from '../../../core/services/theme';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  constructor(public themeService: Theme, public languageService: Language) { }
+
+  scrollTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
+  toggleLanguage() {
+    this.languageService.toggleLanguage();
+  }
+}
