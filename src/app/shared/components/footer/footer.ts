ff5453b45346383485092ddc3a655bdad240db13
Alejandro F. Escobedo Miño <a.franco.escobedo@gmail.com>
2026-04-01 12:26:44 -0300

    arreglo de navegacion de footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { Language } from '../../../core/services/language';
 import { Theme } from '../../../core/services/theme';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -12,9 +13,29 @@ import { Theme } from '../../../core/services/theme';
   styleUrl: './footer.css',
 })
 export class Footer {
-  constructor(public themeService: Theme, public languageService: Language) { }
+  constructor(public themeService: Theme,
+    public languageService: Language,
+    private router: Router) { }
 
+
+    
   scrollTo(section: string) {
+
+    if (this.router.url !== '/') {
+      this.router.navigate(['/']).then(() => {
+
+        setTimeout(() => {
+          document.getElementById(section)?.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }, 150);
+
+      });
+
+      return;
+    }
+
+    // 👉 Si estamos en home
     document.getElementById(section)?.scrollIntoView({
       behavior: 'smooth'
     });
