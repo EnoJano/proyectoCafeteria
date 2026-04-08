48e5a85afa8d4db0673f98bc2b2eba90b9e7f6e2
Alejandro F. Escobedo Miño <a.franco.escobedo@gmail.com>
2026-04-08 12:18:04 -0400

    Componentes de admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { Home } from './pages/home/home';
 import { Menu } from './pages/menu/menu';
 import { NotFound } from './pages/not-found/not-found';
 import { Login } from './pages/admin/login/login';
+import { Dashboard } from './pages/admin/dashboard/dashboard';
+import { Categorias } from './pages/admin/categorias/categorias';
+import { Productos } from './pages/admin/productos/productos';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'menu', component: Menu },
-  { path: 'admin/login', component: Login },
   { path: 'not-found', component: NotFound },
+  { path: 'admin/login', component: Login },
+  { path: 'admin/dashboard', component: Dashboard },
+  { path: 'admin/categorias', component: Categorias },
+  { path: 'admin/productos', component: Productos },
   { path: '**', redirectTo: 'not-found' }
 ];
